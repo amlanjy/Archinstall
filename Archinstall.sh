9b92a7dcb169f4d0943c5da4c403b68bc40d0b40
Amlanjy <amlanjynew@gmail.com>
2026-01-23 03:53:12 +0530

    Create Archinstall.sh

---
+++ Archinstall.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+set -e
+
+clear
+echo "=== MINIMAL ARCH INSTALLER ==="
+
+read -p "Target disk (e.g. /dev/nvme0n1): " DISK
+read -p "Username: " USERNAME
+read -s -p "User password: " USERPASS
+echo
+read -s -p "Root password: " ROOTPASS
+echo
+
+echo "Select timezone region (e.g. Asia, Europe, America):"
+ls /usr/share/zoneinfo
+read -p "Region: " TZ_REGION
+
+echo "Select city (e.g. Kolkata, Berlin, New_York):"
+ls /usr/share/zoneinfo/$TZ_REGION
+read -p "City: " TZ_CITY
+
+TIMEZONE="$TZ_REGION/$TZ_CITY"
+
+echo "Desktop:"
+echo "1) KDE"
+echo "2) GNOME"
+echo "3) XFCE"
+echo "4) LXQt"
+echo "5) Niri"
+echo "6) Hyprland"
+echo "7) Server only"
+read -p "Choice: " DE
+
+timedatectl set-ntp true
+
+cat > /etc/pacman.d/mirrorlist <<EOF
+Server = https://mirror.kumi.systems/archlinux/\$repo/os/\$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
+Server = https://mirror.nus.edu.sg/archlinux/\$repo/os/\$arch
+EOF
+
+wipefs -af "$DISK"
+sgdisk -Z "$DISK"
+sgdisk -n 1:0:+1G -t 1:ef00 "$DISK"
+sgdisk -n 2:0:0   -t 2:8300 "$DISK"
+
+EFI="${DISK}p1"
+ROOT="${DISK}p2"
+
+mkfs.fat -F32 "$EFI"
+mkfs.btrfs -f "$ROOT"
+
+mount "$ROOT" /mnt
+btrfs subvolume create /mnt/@
+umount /mnt
+
+mount -o compress=zstd,subvol=@ "$ROOT" /mnt
+mkdir -p /mnt/boot
+mount "$EFI" /mnt/boot
+
+pacstrap /mnt \
+base linux linux-firmware grub sudo \
+networkmanager \
+btrfs-progs zram-generator \
+mesa libglvnd vulkan-icd-loader \
+pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
+bluez bluez-utils
+
+genfstab -U /mnt > /mnt/etc/fstab
+
+arch-chroot /mnt <<EOF
+set -e
+
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "arch" > /etc/hostname
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+
+cat > /etc/systemd/zram-generator.conf <<ZRAM
+[zram0]
+zram-size = 4G
+compression-algorithm = zstd
+ZRAM
+
+useradd -m -G wheel $USERNAME
+echo "$USERNAME:$USERPASS" | chpasswd
+echo "root:$ROOTPASS" | chpasswd
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+sed -i 's/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="rootflags=subvol=@ quiet"/' /etc/default/grub
+sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=true/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+case $DE in
+1) pacman -S --noconfirm plasma-desktop konsole dolphin sddm && systemctl enable sddm ;;
+2) pacman -S --noconfirm gnome-shell gdm && systemctl enable gdm ;;
+3) pacman -S --noconfirm xfce4 xfce4-terminal lightdm lightdm-gtk-greeter && systemctl enable lightdm ;;
+4) pacman -S --noconfirm lxqt breeze-icons sddm && systemctl enable sddm ;;
+5) pacman -S --noconfirm niri xwayland ;;
+6) pacman -S --noconfirm hyprland xwayland ;;
+7) ;;
+esac
+EOF
+
+umount -R /mnt
+echo "=== DONE. REBOOT ==="
+reboot
